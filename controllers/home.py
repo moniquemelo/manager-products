4cbe024f311308fc8a6c8ca8e026f932e59cc7d6
moniquemelo <jmoniquemelo@gmail.com>
2021-07-09 16:28:19 -0300

    Criação campos do BD e inserção dos dados

--- controllers/home.py
+++ controllers/home.py
@@ -1,11 +1,36 @@
+import json
+
+import requests
 from flask import Blueprint, render_template
 
+from models import Product, base
 
 home = Blueprint('home', __name__)
 
 
 @home.route('/', methods=['GET'])
 def getindex():
+    headers = {'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/'
+                             '91.0.4472.124 Safari/537.36'}
+
+    request = requests.get('https://servicespub.prod.api.aws.grupokabum.com.br/home/v1/home/produto', headers=headers)
+    api = json.loads(request.content)
+
+
+    for product in api['produtos']:
+        prod_code = product['codigo']
+        prod_name = product['nome']
+        prod_price = product['preco']
+        prod_image = product['img']  # verificar se vale a pena
+        prod_brand = product['fabricante']['nome']
+        prod_quantity = 0
+        if 'quantidade' in product['oferta']:
+            prod_quantity = product['oferta']['quantidade']
+
+
+        all_product = Product(prod_code, prod_name, prod_price, prod_image, prod_brand, prod_quantity)
+        base.session.add(all_product)
+        base.session.commit()
     return render_template('home/index.html')
 
 
--- models/product.py
+++ models/product.py
@@ -3,11 +3,20 @@ from models.base import base
 
 class Product(base.Model):
 
-    id = base.Column(base.Integer, primary_key=True)
-    name = base.Column(base.String(128), unique=True, nullable=False)
+    code = base.Column(base.Integer, unique=True, primary_key=True)
+    name = base.Column(base.String(128), nullable=False)
+    price = base.Column(base.DECIMAL(10, 2), nullable=False)
+    image = base.Column(base.String(255))
+    brand = base.Column(base.String(128))
+    quantity = base.Column(base.Integer, nullable=False)
 
-    def __init__(self, name):
+    def __init__(self, code, name, price, image, brand, quantity):
+        self.code = code
         self.name = name
+        self.price = price
+        self.image = image
+        self.brand = brand
+        self.quantity = quantity
 
     def __repr__(self):
         return self.name
